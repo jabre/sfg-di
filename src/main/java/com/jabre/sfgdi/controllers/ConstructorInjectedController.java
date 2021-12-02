1a0dd9bd5fb49067b2b801f3955807b03a1ae23b
jabre <marko13r@gmail.com>
2021-12-02 19:39:48 +0100

    add manual DI samples

--- src/main/java/com/jabre/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/jabre/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,13 @@
 package com.jabre.sfgdi.controllers;
 
 import com.jabre.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
 /**
  * @author : Jabre
  * @created : 12/1/2021, Wednesday
  **/
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/com/jabre/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/com/jabre/sfgdi/services/GreetingServiceImpl.java
@@ -1,9 +1,12 @@
 package com.jabre.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author : Jabre
  * @created : 12/1/2021, Wednesday
  **/
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
